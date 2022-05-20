4639f4349e1dcf354ce31324c61633bcf2e45fa3
Lyss Garcia <alyssa.g200@gmail.com>
2022-05-20 16:50:08 -0700

    added react

--- assets/js/script.js
+++
@@ -1,9 +0,0 @@
-// search button event
-$('#submit-prompt-btn').click(function(e) {
-    if ($('#user-prompt').val()) {
-        e.preventDefault()
-        console.log('submit button clicked')
-        outputField.text('')
-        $('#user-prompt').val('') //clears search field
-    }
-})
